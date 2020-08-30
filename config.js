6cd9ecd6f4dc031f3dc3346ac1d934548991c217
Lauren Sanders <66180981+mlsterl1@users.noreply.github.com>
2020-08-30 06:24:12 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
-pk.eyJ1IjoibWxzdGVybDEiLCJhIjoiY2tlZjFiY3pqMDlieTJ5bnZ0YzQxdHg1NiJ9.JZDWbrFAex3myAjWqhf6LA
+// API key
+const API_KEY = "pk.eyJ1IjoibWxzdGVybDEiLCJhIjoiY2tlZjFiY3pqMDlieTJ5bnZ0YzQxdHg1NiJ9.JZDWbrFAex3myAjWqhf6LA";
